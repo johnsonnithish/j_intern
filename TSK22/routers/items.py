890bfe14c05f28157d5ad1a62308d23a4d485af0
Johnson <johnson.m@oasyscybernetics.in>
2025-05-05 13:01:32 +0530

    VALS_ADDED

--- TSK22/routers/items.py
+++ TSK22/routers/items.py
@@ -4,9 +4,15 @@ router = APIRouter(prefix="/items", tags=["items"])
 
 @router.get("/")
 def read_items():
-    return {"message": "ITEMS"}
+    return [
+            {"it_id": 1, "it_name": "item1"},
+            {"it_id": 2, "it_name": "item2"},
+            {"it_id": 3, "it_name": "item3"}
+            ]
 
 @router.get("/{item_id}")
 def read_item_id(item_id: int):
-    return {"item": f"Item {item_id}"}
+    return [
+            {"it_id": item_id, "it_name": f"item{item_id}"}
+         ]
 
--- TSK22/routers/users.py
+++ TSK22/routers/users.py
@@ -4,8 +4,14 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/")
 def read_users():
-    return {"message": "USERS"}
+    return [
+            {"user_id": 1, "user_name": "user1"},
+            {"user_id": 2, "user_name": "user2"},
+            {"user_id": 3, "user_name": "user3"}
+            ]
 
 @router.get("/{user_id}")
 def read_user_id(user_id: int):
-    return {"user": f"User {user_id}"}
+    return [
+            {"user_id": user_id, "user_name": f"user{user_id}"}
+            ]
